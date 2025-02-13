dd57b0f429ccac7a9ac28072a43ecba024380328
aadit11 <aadit.kshirsagar@gmail.com>
2025-02-13 08:40:26 +0530

    Added more files

---
+++ momentum.py
@@ -0,0 +1,8 @@
+mass = float(input("Enter the mass"))
+velocity = float(input("Enter the velocity"))
+
+mmntem = 0
+
+mmntem = mass * (velocity * velocity)
+
+print("The momentum of the body is ", mmntem)
