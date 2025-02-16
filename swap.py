5ccb2e8ab799395870b1d869f1448d4969eb4b7b
aadit11 <aadit.kshirsagar@gmail.com>
2025-02-16 13:32:56 +0530

    Added more files

---
+++ swap.py
@@ -0,0 +1,12 @@
+a = int(input("enter value for a"))
+b = int(input("enter value for b"))
+
+temp = 0
+
+
+temp = a
+a = b
+b = temp
+
+print("The new value for a is ", a)
+print("The new value for b is ", b)
